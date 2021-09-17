ae114893b78e83e40b56430710ce77ba8aa321c0
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-09-17 00:39:39 -0300

    fixing get getStaticProps to revalidate and routing to previews

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -86,6 +86,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
-    redirect: 60 * 30, // 30 minutes
+    revalidate: 60 * 30, // 30 minutes
   };
 };
